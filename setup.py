09e4370eab994a9c32c0fd059378975e40b55380
vivekkarnakar <karnakarvivek@gmail.com>
2025-06-12 15:40:35 +0530

    all requirements installed and setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "us_visa",
+    version = "0.0.0",
+    author="Vivek",
+    author_email="karnakarvivek@gmail.com",
+    packages = find_packages()
+)
